2dfcf8906422dd8b2eb1f302837f5ac77b7e1b04
Manxprince <Majolaprince71@gmail.com>
2022-05-31 23:37:24 +0200

    my first commit

---
+++ dart file 1.dart
@@ -0,0 +1,8 @@
+void main() {
+  String name = "Prince Nkosinathi Majola";
+  String favoriteApp = "metatrader 5";
+  String city = "Pretoria";
+  print("my name is $name");
+  print("my favorite app is $favoriteApp");
+  print("my city is $city");
+}
---
+++ dart file 2.dart
@@ -0,0 +1,22 @@
+void main() {
+  var apptypes = [
+    'Amabani-21',
+    'EasyEquities-20',
+    'Naked insurance-19',
+    'Khula-18',
+    'Standard Bank shyft-17',
+    'Domestly-16',
+    'WumDrop-15',
+    'Live Inspect-14',
+    'SnapScan-13',
+    'FNB Banking-12',
+  ];
+
+  print(apptypes);
+
+  // printing the winning apps for 2017 and 2018
+
+  print(apptypes[3]);
+  print(apptypes[4]);
+  apptypes.sort();
+}
